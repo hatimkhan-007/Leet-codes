5bbf7a0a9346e12197d7d7101ddb6dbc71706602
Uzair Khan <uzairkhan60621@gmail.com>
2026-08-23 00:57:47 +0500

    Adding code to find the divisible for the sum and product of the number.

---
+++ divisiblebysumandproduct.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool checkDivisibility(int n) {
+        int sum = 0;
+        int product = 1;
+        int temp = n;
+        while(temp > 0){
+            int rem = temp % 10;
+            sum += rem;
+            product *= rem;
+            temp = temp / 10;
+        }
+        
+        return n % (sum + product) == 0;
+    }
+};
